9ae6920b72b21eb9429a49e06911f17696f2f094
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-03-22 23:46:21 +0500

    Annimation added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lottie Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Lottie Demo'),
@@ -44,8 +36,13 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Lottie.asset(
+          "assets/splash_anim.json",
+          controller: null,
+          repeat: true,
+          
+        ),
+      ),
     );
   }
 }
